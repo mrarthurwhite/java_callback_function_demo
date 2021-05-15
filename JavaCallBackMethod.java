1393e9de8006feb45901e9bcbcfb0c1f47d73221
mrarthurwhite <mister.arthur.white@gmail.com>
2021-05-15 06:35:50 -0400

    updating & simplifying

--- JavaCallBackMethod.java
+++ JavaCallBackMethod.java
@@ -1,26 +1,20 @@
 //predicate = pre:before dicate:make known
-interface MyDiisplayerInterface {
-	String stringFunction(String str);
+interface MyDisplayerInterface {
+	void display(int number);
 }
 
 
 public class JavaCallBackMethod {
 
-	public static String myDisplayer(int number) {
-		System.out.println("Displaying number: " + number)
+	public static void myDisplayer(int number) {
+		System.out.println("Displaying number: " + number);
 	}
 	
-	public static void myCalculator( int number, MyDiisplayerInterface myCallback){
-	   myCallback.stringFunction(number);
+	public static void myCalculator( int number, MyDisplayerInterface myCallback){
+	   myCallback.display(number);
 	}
 	
 	public static void main (String args[]) {
-		// lambda to reverse string
-		MyDiisplayerInterface reverse = (str) -> {		
-			return new StringBuffer(str).reverse().toString();
-		};
-
-		// 
-		myCalculator( 100, reverse); 
+		myCalculator( 100, JavaCallBackMethod::myDisplayer); 
 	}
 }
